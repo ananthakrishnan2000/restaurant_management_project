3eb3032d7654b563847fa26de111ada72978b54e
ananthakrishnan2000 <123975615+ananthakrishnan2000@users.noreply.github.com>
2025-08-09 16:09:27 +0530

    23989

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -1,22 +1,4 @@
-from django.conf import settings
-from django.http import HttpResponse
-from django.urls import path
-from django.core.wsgi import get_wsgi_application
-
-settings.configure(
-    DEBUG=True,
-    SECRET_KEY='abc123',
-    ROOT_URLCONF=__name__,
-    ALLOWED_HOSTS=['*'],
-    RESTAURANT_NAME="My Awesome Restaurant",
-
-)
-
-def home(request):
-    return HttpResponse(f"<h1>Welcome to {settings.RESTAURANT_NAME} </h1>")
-urlpatterns = [
-    path('', home),
-
-]
-
-application = get_wsgi_application()
+def great():
+    return "Hello from python!"
+ 
+print(great())
